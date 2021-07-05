f785143475b2e55781608c789cb91d02aa5aa112
Gabriel <patino-marcos@outlook.com>
2021-07-05 12:30:40 -0300

    repaso de metodos de array

---
+++ app.js
@@ -0,0 +1,13 @@
+let numeros = [ 7, 3, 5, 7, 3, 3, 4, 356 ,6, 56 ]
+
+let masCinco = numeros.map( (numero) => numero * 3 )
+//[21, 9]
+
+
+ let menoresADiez = numeros.filter( (numero) => {
+    numero > 100000
+ })
+//console.log(menoresADiez)
+menoresADiez ? console.log(menoresADiez) : null
+
+
