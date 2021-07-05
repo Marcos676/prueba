0b536ec6a70de48eebf8eeb0c9f04dffe33b4303
Gabriel <patino-marcos@outlook.com>
2021-07-05 12:44:34 -0300

    cambios en app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let numeros = [ 7, 3, 5, 7, 3, 3, 4, 356 ,6, 56 ]
+let numeros = [ 7, 3 ,6, 56 ]
 
 let masCinco = numeros.map( (numero) => numero * 3 )
 
@@ -6,8 +6,9 @@ let masCinco = numeros.map( (numero) => numero * 3 )
  let menoresADiez = numeros.filter( (numero) => {
     numero > 100000
  })
-console.log(menoresADiez)
+
 menoresADiez ? console.log(menoresADiez) : null
 
 console.log(numeros);
 
+console.log(menoresADiez)
